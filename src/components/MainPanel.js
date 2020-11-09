143e35f0b867954cf102dbc325fa784ab2822ae0
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-11-09 16:28:47 -0500

    [UPDATE]Panel layout started

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -3,9 +3,36 @@ import { SideClientsNotification } from './SideClientsNotification'
 
 export const MainPanel = () => {
   return (
-    <div className="main-panel__container">
-      mainPanel
+    <article className="main-panel__container">
+      <main className="main-panel__main">
+      <header className="main-panel__header">
+        <input type="search" className="main-panel__header--input" placeholder="Search..." />
+        <p>Wed, 20 May</p>
+      </header>
+      <h1 className="main-panel__title">
+        Hola, Jorge
+      </h1>
+
+      <div className="main-panel__top">
+        <section>
+          Cola de llamadas
+        </section>
+        <section>
+          Meta grupal
+        </section>
+      </div>
+      <div className="main-panel__bottom">
+        <section>
+          Conexiones de agenda
+        </section>
+        <section>
+          Tu plan de hoy
+        </section>
+      </div>
+
+
+      </main>
       <SideClientsNotification />
-    </div>
+    </article>
   )
 }
