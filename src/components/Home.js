6b5439a7ac8600b6ae5c64ef9e5f8dfb32a9fb71
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-11-10 06:51:08 -0500

    [UPDATE]input name home working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
 
 export const Home = () => {
+
+const [userName, setUserName] = useForm({
+  name:''
+})
+const { name } = userName;
+
+console.log(name)
   return (
-    <div className="animate__animated animate__fadeIn" >
-      Home
+    <div className="home__container animate__animated animate__fadeIn" >
+
+      <h1 className="home__title text-center"> Bienvenido a Kuepa CMR </h1>
+      <h3 className="home__subtitle text-center"> Te deseamos un excelente dia </h3>
+
+      <form className='home__input--container'>
+        <label>
+          ¿Cual es tu nombre?
+        </label>
+        <input
+          value={name}
+          name="name"
+          className="home__input-name"
+          type="text"
+          placeholder="Camila..."
+          autoComplete="off"
+          onChange={setUserName}
+        />
+      </form>
     </div>
   )
 }
--- src/types/types.js
+++ src/types/types.js
@@ -1,4 +1,6 @@
 export const types = {
   clientsLoaded: '[Client] clients loaded ',
-  clientDetailLoaded: '[Client] client details loaded'
+  clientDetailLoaded: '[Client] client details loaded',
+
+  userNameSaved: '[User] User name added'
 }
