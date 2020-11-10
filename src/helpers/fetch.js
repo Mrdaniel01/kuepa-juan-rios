1e74ddc0ca1b29cadf5b534dd39d7eee9e2a1dd5
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-11-10 11:22:16 -0500

    [UPDATE]custom enviroment variable added

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,14 +1,14 @@
-//const baseUrl = process.env.REACT_APP_API_URL;
+const baseUrl = process.env.REACT_APP_API_URL;
 
 export const getClients = async () => {
-  const url = `https://api.opendota.com/api/proPlayers`;
+  const url = `${baseUrl}/proPlayers`;
   const resp = await fetch(url);
   const clients = await resp.json();
   return clients;
 };
 
 export const getClientById = async (id) => {
-  const url = `https://api.opendota.com/api/players/${id}`;
+  const url = `${baseUrl}/players/${id}`;
   const resp = await fetch(url);
   const client = await resp.json();
   return client;
